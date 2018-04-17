2fe2d4f65aa89d3fbb9b6ab5f218a3475c6c62fe
jjade seravla <jadealvares@hotmail.co.uk>
2018-04-17 15:56:15 +0100

    nested message component added

--- WelcomeToVR/index.vr.js
+++ WelcomeToVR/index.vr.js
@@ -11,8 +11,8 @@ export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
+        <Pano source={asset('test.jpg')}/>
+        {/* <Text
           style={{
             backgroundColor: '#777879',
             fontSize: 0.8,
@@ -24,9 +24,31 @@ export default class WelcomeToVR extends React.Component {
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
           }}>
-          hello
-        </Text>
+          Click here
+        </Text> */}
+         <NestedMessage message={"Hello Nested Message"}/>
       </View>
+    );
+  }
+};
+
+class NestedMessage extends React.Component {
+render() {
+    return (
+      <Text
+        style={{
+          backgroundColor: '#777879',
+          fontSize: 0.8,
+          fontWeight: '400',
+          layoutOrigin: [0.5, 0.5],
+          paddingLeft: 0.2,
+          paddingRight: 0.2,
+          textAlign: 'center',
+          textAlignVertical: 'center',
+          transform: [{translate: [0, 0, -3]}],
+        }}>
+        {this.props.message}
+      </Text>
     );
   }
 };
